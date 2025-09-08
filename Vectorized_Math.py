d8e724e14865dce4f5bd6174a1bdc686cf87dcb2
Tejaswi <sudharaniburra66@gmail.com>
2025-09-08 15:10:06 +0530

    Added vectorized math operations with NumPy (addition, subtraction, multiplication, division, power, modulus)

---
+++ Vectorized_Math.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr_1 = np.array([1, 2, 3, 4, 5])
+arr_2 = np.array([2,3,4,5,6])
+print(arr_1 + arr_2)
+print(arr_1 - arr_2)
+print(arr_1 * arr_2)
+print(arr_1 / arr_2)
+print(arr_1 ** arr_2)
+print(arr_1 % arr_2)
